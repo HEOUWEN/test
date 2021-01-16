22bd51df20db9312997d94c35c296bfcf116a1e5
HEOUWEN <heouwen3333@gmail.com>
2021-01-16 21:04:25 +0800

    day_03

---
+++ test_1_16/test_1_16/test.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+//int main()
+//
+//{
+//	printf("%c", a);
+//	return  0;
+//}
+
+//int main()
+//{
+//	char arr[] = {73,32,99,97,110,32,100,111,32,105,116,33,\0};
+//	int i = 0;
+//	for (i = 0; i<12; i++)
+//	{
+//		printf("%c", arr[i]);
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int date = 0;
+//	scanf("%4d%2d%2d", &year, &month, &date);
+//		printf("year = %4d\nmonth = %02d\ndate = %02d", year, month, date);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char n = 0;
+//	while (scanf("%c",&n) != EOF)
+//	{
+//		printf("%c\n", 1 << n);
+//	}
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	char n = 0;
+//	while ((scanf("%c",&n)) != EOF)
+//	{
+//		if ((n >= 'a') && (n <= 'z'))
+//			printf("YES\n");
+//		else
+//			printf("NO\n");
+//		getchar();
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	while ((scanf("%d", &n) != EOF))
+//	{
+//		printf("%d\n", 1 << n);
+//	}
+//	return 0;
+//
+//}
+
+//1.
+//int main()
+//{
+//	char arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33 };
+//	int i = 0;
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("%c", arr[i]);
+//	}
+//	return 0;
+//}
+//
+//2.
+//int main()
+//{
+//	int y = 0;
+//	int m = 0;
+//	int d = 0;
+//
+//	scanf("%4d%2d%2d", &y, &m, &d);
+//	printf("year=%04d\n", y);
+//	printf("month=%02d\n", m);
+//	printf("date=%02d\n", d);
+//
+//	return 0;
+//}
+
+//3.
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		printf("%d\n", 1 << n);
+//	}
+//	return 0;
+//}
