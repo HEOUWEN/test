0b80c0ee77900236ac3cf2e90d834b020ecde951
HEOUWEN <heouwen3333@gmail.com>
2021-01-18 11:12:10 +0800

    day04

---
+++ test_1_17_3/test_1_17/test.c
@@ -0,0 +1,193 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+//int main()
+//{
+//	int ch = 0;
+//	scanf("%d", &ch);
+//	printf("%c\n", ch);
+//	return 0;
+//
+//}
+//
+//int main ()
+//{
+//    int a = 40;
+//    int c = 212;
+//    printf("(-8+22)*%d-10+%d/2",a,c);
+//    return 0 ;
+//}
+//
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//
+//	scanf("%d %d", &a, &b);
+//
+//	printf("%d %d", a/b, a%b);
+//	return 0;
+//
+//}
+//
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	float c = 0.0;
+//	c = a / ((b / 100)*(b / 100));
+//	printf("%.2f", c);
+//	return 0;
+//}
+
+//int main()
+//
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//
+//	scanf("%d,%d,%d", &a, &b, &c);
+//	float z = a + b + c;
+//	float p = (a + b + c) / 2.0;
+//	float s = sqrt(p*(p - a)*(p - b)*(p - c));
+//
+//	printf("circumference=%.2f area=%.2f", z, s);
+//
+//	return 0;
+//}
+
+//#include <math.h>
+//int main()
+//
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//
+//	scanf("%d %d %d", &a, &b, &c);
+//	double z = a + b + c;
+//	double  p = (a + b + c) / 2.0;
+//	double  s = sqrt(p*(p - a)*(p - b)*(p - c));
+//
+//	printf("circumference=%.2lf area=%.2lf", z, s);
+//
+//	return 0;
+//}
+
+
+//
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	float len = a + b + c;
+//	float p = (a + b + c) / 2.0;
+//	float area = sqrt(p*(p - a)*(p - b)*(p - c));
+//	printf("circumference=%.2f area=%.2f\n", len, area);
+//	return 0;
+//}
+//
+//
+////1.
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//
+//	scanf("a=%d,b=%d", &a, &b);
+//	int c = 0;
+//	c = a;
+//	a = b;
+//	b = c;
+//
+//	printf("a=%d,b=%d", a, b);
+//	return 0;
+//}
+//
+//2.
+//int main()
+//{
+//	char a = 0;
+//	a = getchar();
+//	printf("%d", a);
+//	return 0;
+//}
+//
+//3.
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//
+//	int d = (-8 + 22)*a - 10 + c / 2;
+//	printf("%d", d);
+//	return 0;
+//}
+//
+//4.
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d", a / b, a%b);
+//	return 0;
+//
+//}
+
+//5.
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	
+//	scanf("%d %d", &a, &b);
+//	double  c = a / ((b / 100.0)*(b / 100.0));
+//	printf("%.2lf", c);
+//	return 0;
+//}
+
+
+//6.
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	double cir = 0.0;
+//	cir = a + b + c;
+//	double p = (a + b + c) / 2.0;
+//
+//	double area = 0.0;
+//	area = sqrt(p*(p - a)*(p - b)*(p - c));
+//	printf("circumference=%.2lf area=%.2lf", cir, area);
+//
+//	return 0;
+//}
+
+//7.
+//int main()
+//{
+//	double r = 0.0;
+//	scanf("%lf", &r);
+//
+//	double pi = 3.1415926;
+//	double v = 0.0;
+//	v = (4.0 / 3.0)*pi*r*r*r;
+//
+//	printf("%.3lf", v);
+//	return 0;
+//}
+
+//end
