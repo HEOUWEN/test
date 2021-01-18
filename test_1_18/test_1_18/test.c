c77cdac7385c6d8a9da0df4334f9cd9e340d12b3
HEOUWEN <heouwen3333@gmail.com>
2021-01-18 22:00:55 +0800

    day05

---
+++ test_1_18/test_1_18/test.c
@@ -0,0 +1,157 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+//
+//int main()
+//{
+//	int i = 0;
+//
+//	for (i = 10000; i < 100000; i++)
+//	{
+//
+//		int j = 0;
+//		int sum = 0;
+//		for (j = 10; j < 100000; j = j * 10)
+//		{
+//			sum = sum + ((i / j) * (i%j));
+//		}
+//		if (sum  == i)
+//
+//			printf("%d ", i);
+//
+//
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int ret = 0;
+//	scanf("%d %d", &a, &b);
+//	ret = (a % 100 + b % 100) % 100;
+//	print("%d", ret);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	float a = 0.0;
+//
+//	scanf("%f", &a);
+//	double b = (double)a;
+//	printf("%lf", b);
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int i = 0;
+//	long long ret = a*3.156 * 10;
+//	for (i = 1; i <= 7; i++)
+//	{
+//		ret = ret*i;
+//	}
+//	printf("%lld", ret);
+//
+//	return 0;
+//}\
+
+//
+//1.
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("score1=%d,score2=%d,score3=%d", a, b, c);
+//	return 0;
+//}
+
+//2.
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 10000; i < 100000; i++)
+//	{
+//
+//		int sum = 0;
+//		for (j = 10; j < 100000; j = j * 10)
+//		{
+//			sum = sum + ((i / j)*(i%j));
+//
+//		}
+//		if (sum == i)
+//			printf("%d ", i);
+//
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//
+//	for (i = 10000; i < 100000; i++)
+//	{
+//
+//		int j = 0;
+//		int sum = 0;
+//		for (j = 10; j < 100000; j = j * 10)
+//		{
+//			sum = sum + ((i / j) * (i%j));
+//		}
+//		if (sum  == i)
+//
+//			printf("%d ", i);
+//
+//
+//	}
+//	return 0;
+//}
+
+//3.
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int ret = (a % 100 + b % 100) % 100;
+//	printf("%d", ret);
+//	return 0;
+//}
+
+//4.
+//int main()
+//{
+//	double a = 0.0;
+//	scanf("%lf", &a);
+//	int b = 0;
+//	b = (int)a;
+//	printf("%d", b % 10);
+//	return 0;
+//
+//}
+
+//5.
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	long long b = 0;
+//	b = a *  3.156 * 10000000;
+//	printf("%lld", b);
+//	return 0;
+//}
